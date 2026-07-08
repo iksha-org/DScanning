92b73cb2d49baf2ae02b32bc9fc65afb80c06aa0
sowkhyasri <ikshatelkar27@gmail.com>
2026-07-08 15:15:21 +0530

    add db module

--- db.py
+++ db.py
@@ -3,5 +3,6 @@ import sqlite3
 def get_user(user_id):
     conn = sqlite3.connect("app.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+    query = "SELECT * FROM users WHERE id = %s" % user_id
+    cur.execute(query)
     return cur.fetchone()
